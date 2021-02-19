17656b5e015c49a94fbcac14195304232cdcf2fa
angus <316659146@qq.com>
2021-02-20 00:21:28 +0800

    初次使用cglib动态代理

--- monster/foodie-service/src/main/java/com/angus/service/angusServiceImpl.java
+++ monster/foodie-service/src/main/java/com/angus/service/angusServiceImpl.java
@@ -9,7 +9,18 @@ package com.angus.service;
  */
 public class angusServiceImpl implements angusService{
     @Override
+    /**
+     * @title talk
+     * @description
+     * @author admin []
+     * @updateTime 2021/2/20 [] void
+     * @throws 用于jdk代理使用
+     */
     public void talk() {
         System.out.println("I'am angus");
+    }
+
+    public  void selfTalk(){
+        System.out.println("cglib use !");
     }
 }
