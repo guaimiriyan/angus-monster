8e9bc816d546f2463beb973cef42b950d0b56aea
angus <316659146@qq.com>
2021-03-17 09:22:51 +0800

    适配器模式学习

---
+++ designModel/src/main/java/h_AdapterModel/loginadapter/v2/adapters/LoginForTokenAdapter.java
@@ -0,0 +1,16 @@
+package h_AdapterModel.loginadapter.v2.adapters;
+
+
+import h_AdapterModel.loginadapter.ResultMsg;
+
+/**
+ * Created by Tom.
+ */
+public class LoginForTokenAdapter implements LoginAdapter {
+    public boolean support(Object adapter) {
+        return adapter instanceof LoginForTokenAdapter;
+    }
+    public ResultMsg login(String id, Object adapter) {
+        return null;
+    }
+}
