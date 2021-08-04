87f44bdc50d0b60525124f1228ab9c2747f80bad
angus <316659146@qq.com>
2021-08-04 18:46:17 +0800

    计数排序和基数排序的实现

---
+++ base/src/main/java/sort/bucket_sort.java
@@ -0,0 +1,14 @@
+package sort;
+
+/**
+ * @author angus
+ * @version 1.0.0
+ * @ClassName bucket_sort.java
+ * @Description 使用桶的方式进行排序，计数排序和基数排序都是基本使用的桶排序
+ * @createTime 2021年08月04日 15:26:00
+ */
+public class bucket_sort {
+    public static void main(String[] args) {
+        final int[] datas = RandomUtil.getRandomIntArray(100000, 1000);
+    }
+}
